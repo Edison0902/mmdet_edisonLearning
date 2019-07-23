0aa29e2d7ec924d4bf35e33778c0dcf361c80a18
Edison0902 <457716625@qq.com>
2019-07-23 11:01:34 +0800

    update compile.sh

--- mmdetection/build/lib/mmdet/models/backbones/__init__.py
+++
@@ -1,9 +0,0 @@
-from .resnet import ResNet, make_res_layer
-from .resnext import ResNeXt
-from .ssd_vgg import SSDVGG
-from .hrnet import HRNet
-from .densenet import DenseNet
-from .res_net import ResNet_CJH
-from .dense_net import DenseNet_CJH
-
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'DenseNet', 'ResNet_CJH', 'DenseNet_CJH']
--- mmdetection/build/lib/mmdet/models/necks/__init__.py
+++
@@ -1,5 +0,0 @@
-from .fpn import FPN
-from .bfp import BFP
-from .hrfpn import HRFPN
-
-__all__ = ['FPN', 'BFP', 'HRFPN']
